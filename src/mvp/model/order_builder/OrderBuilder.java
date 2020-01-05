8b0e9f328872cb56d6033e828f3bd7d237f2c800
Robert Moldoveanu <robert.moldoveanu97@gmail.com>
2020-01-05 17:30:47 +0200

    declared payment strategy as a property

--- src/mvp/model/order_builder/OrderBuilder.java
+++ src/mvp/model/order_builder/OrderBuilder.java
@@ -6,10 +6,12 @@ import mvp.model.Order;
 import mvp.model.OrderItem;
 import mvp.model.Product;
 import mvp.model.User;
+import mvp.model.strategy.PaymentStrategy;
 
 public class OrderBuilder {
 	private User client;
 	private ArrayList<OrderItem> orderList;
+	private PaymentStrategy paymentStrategy;
 	
 	public OrderBuilder() {
 		this.orderList = new ArrayList<OrderItem>();
@@ -18,15 +20,20 @@ public class OrderBuilder {
 	public OrderBuilder from(User client) {
 		this.client = client;
 		return this;
-	}
+	}	
 	
 	public OrderBuilder addProduct(Product product, int quantity) {
 		this.orderList.add(new OrderItem(product, quantity));
 		return this;
 	}
 	
+	public OrderBuilder paymentAs(PaymentStrategy paymentStrategy) {
+		this.paymentStrategy = paymentStrategy;
+		return this;
+	}
+	
 	public Order getOrder() {
-		Order order = new Order(client, orderList);
+		Order order = new Order(client, orderList, paymentStrategy);
 		return order;
 	}
 }
