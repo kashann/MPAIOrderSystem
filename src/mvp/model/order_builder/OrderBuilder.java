6c238ee0dffd7bb50128a734e181ef9568957cd7
Robert Moldoveanu <robert.moldoveanu97@gmail.com>
2020-01-05 16:46:55 +0200

    removed payment and delivery params from getOrder in OrderBuilder p.2

--- src/mvp/model/order_builder/OrderBuilder.java
+++ src/mvp/model/order_builder/OrderBuilder.java
@@ -2,8 +2,6 @@ package mvp.model.order_builder;
 
 import java.util.HashMap;
 
-import mvp.model.EDelivery;
-import mvp.model.EPayment;
 import mvp.model.Order;
 import mvp.model.Product;
 import mvp.model.User;
@@ -11,8 +9,6 @@ import mvp.model.User;
 public class OrderBuilder {
 	private User client;
 	private HashMap<Product, Integer> orderList;
-	private EPayment paymentMethod; //transformam in interfata
-	private EDelivery deliveryMethod; //transformam in interfata
 	
 	public OrderBuilder() {
 		this.orderList = new HashMap<Product, Integer>();
@@ -25,16 +21,6 @@ public class OrderBuilder {
 	
 	public OrderBuilder addProduct(Product product, int quantity) {
 		this.orderList.put(product,  quantity);
-		return this;
-	}
-	
-	public OrderBuilder withPayment(EPayment paymentMethod) {
-		this.paymentMethod = paymentMethod;
-		return this;
-	}
-	
-	public OrderBuilder withDelivery(EDelivery deliveryMethod) {
-		this.deliveryMethod = deliveryMethod;
 		return this;
 	}
 	
